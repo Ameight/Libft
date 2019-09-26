c027ee35cf62a7a677296ebe873ded3c0a9c30e2
Ellsworth Juana <ejuana@at-j5.21-school.ru>
2019-09-27 00:56:00 +0300

    Fixed all

---
+++ ft_strjoin.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ejuana <ejuana@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/23 19:17:01 by ejuana            #+#    #+#             */
+/*   Updated: 2019/09/25 14:28:48 by ejuana           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *tmp;
+
+	if (!s1 || !s2)
+		return (NULL);
+	if ((!(tmp = ft_strnew(sizeof(char) * (ft_strlen((char *)s1) +
+		ft_strlen((char *)s2))))))
+		return (NULL);
+	tmp = ft_strcpy(tmp, s1);
+	if (!(tmp = ft_strcat(tmp, (char *)s2)))
+		return (NULL);
+	return (tmp);
+}
